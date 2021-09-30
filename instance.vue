3196d528f68a54e3677c6063366a58b6c4aaf171
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-30 16:54:10 -0300

    Create instance.vue

---
+++ instance.vue
@@ -0,0 +1,8 @@
+import { getCurrentInstance } from "vue";
+export default {
+  setup() {
+    // accessible inside setup function
+    const instance = getCurrentInstance();
+    console.log(instance);
+  },
+};
